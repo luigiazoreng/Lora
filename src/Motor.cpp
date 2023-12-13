bec2262686a1a71987ba0005acaa8062c9efd9ea
luigiazoreng <luigiazoreng@gmail.com>
2023-12-13 00:04:29 -0300

    Motor Working

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -21,35 +21,36 @@ void Motor::set()
 void Motor::forward()
 {
     // going foward
-    digitalWrite(pinoMotor1, LOW);
-    digitalWrite(pinoMotor2, HIGH);
-    digitalWrite(pinoMotor3, LOW);
-    digitalWrite(pinoMotor0, HIGH);
+
+    digitalWrite(pinoMotor1, HIGH);
+    digitalWrite(pinoMotor2, LOW);
+    digitalWrite(pinoMotor3, HIGH);
+    digitalWrite(pinoMotor0, LOW);
 }
 void Motor::backward()
 {
     // going backwards
     digitalWrite(pinoMotor1, LOW);
-    digitalWrite(pinoMotor2, LOW);
-    digitalWrite(pinoMotor3, HIGH);
+    digitalWrite(pinoMotor2, HIGH);
+    digitalWrite(pinoMotor3, LOW);
     digitalWrite(pinoMotor0, HIGH);
 }
 
 void Motor::left()
 {
     // going left
-    digitalWrite(pinoMotor1, HIGH);
-    digitalWrite(pinoMotor2, HIGH);
-    digitalWrite(pinoMotor3, LOW);
-    digitalWrite(pinoMotor0, LOW);
+    digitalWrite(pinoMotor1, LOW);
+    digitalWrite(pinoMotor2, LOW);
+    digitalWrite(pinoMotor3, HIGH);
+    digitalWrite(pinoMotor0, HIGH);
 }
 
 void Motor::right()
 {
     // going right
     digitalWrite(pinoMotor1, HIGH);
-    digitalWrite(pinoMotor2, LOW);
-    digitalWrite(pinoMotor3, HIGH);
+    digitalWrite(pinoMotor2, HIGH);
+    digitalWrite(pinoMotor3, LOW);
     digitalWrite(pinoMotor0, LOW);
 }
 void Motor::stop()
